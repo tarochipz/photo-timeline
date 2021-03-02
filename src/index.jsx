0fdd33ef9e0960fffb26a31a33ad9b8e4667adc3
Sylvia Cung <sylvia.cung@liveramp.com>
2021-03-01 23:47:09 -0800

    title and stuff

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Timeline from './timeline.jsx';
+import './index.css'
 
 class App extends React.Component {
 	render() {
 		return (
 			<>
-				<div>Happy 4 years</div>
+				<h1 class='pageHeader'>The Adventures of Sylv & Rav</h1>
 				<Timeline />
+        {/* <h2 class='pageHeader'>Can't wait for more adventures.</h2> */}
 			</>
 		);
 	}
