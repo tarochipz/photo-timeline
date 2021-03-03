c3059755fe79a940ce1a23738971fbe2c723625f
Sylvia Cung <sylvia.cung@liveramp.com>
2021-03-03 02:07:20 -0800

    yay done

--- src/index.jsx
+++ src/index.jsx
@@ -18,7 +18,15 @@ class App extends React.Component {
 					<FontAwesomeIcon icon={faHeart} />
 				</div>
 				<Timeline />
-				{/* <h2 class='pageHeader'>Can't wait for more adventures.</h2> */}
+				<div class="footer">
+					Yay you finally made it til the end.<br />
+					I know I'm awesome most of the time, but I can also be pretty annoying to deal with.<br />
+					I appreciate you being part of my life the past 4 years and being there for me when it matters.<br />
+					Can't wait for more adventures with you when Covid is over <br /><br />
+					<strong>
+						Happy 4 years mr.Wav, I love you <FontAwesomeIcon icon={faHeart} />
+					</strong>
+				</div>
 			</>
 		);
 	}
